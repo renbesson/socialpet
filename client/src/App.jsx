bcb11d60a7f30ce9d78fe5422299c7ba4b2c44a8
Ren Besson <ren.besson@outlook.com>
2023-04-13 01:55:24 -0300

    React updated to 18 and react-router modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,44 @@
-// import logo from './logo.svg';
-import "./App.css";
-import LandingPage from "./components/landingPage";
-import Login from "./components/login";
-import SignUp from "./components/signup";
-import FeedCard from "./components/feedCard";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './App.css';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import FeedCard from './components/feedCard';
 // import ProfilePage from './components/profilePage'
-// import Feed from
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/profile/:id",
-    element: <SignUp />,
-  },
-  {
-    path: "/feedcard",
-    element: (
-      <FeedCard
-        pet={{ Name: "Cat" }}
-        content="Cat content"
-        imageUrl="https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1286&q=80"
-      />
-    ),
-  },
-]);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ResponsiveAppBar from './components/AppBar';
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <ResponsiveAppBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            {/* <Route path="/profile/:profileId" element={<Profile />} /> */}
+            <Route
+              path="/feedcard"
+              element={
+                <FeedCard
+                  pet={{ Name: 'Cat' }}
+                  content="Cat content"
+                  imageUrl="https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1286&q=80"
+                />
+              }
+            />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
