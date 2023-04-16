5d9355cadc6bd5aabae57a3d890ddb6f277239c5
Ren Besson <ren.besson@outlook.com>
2023-04-16 18:05:16 -0300

    Added postRoutes.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const petRoutes = require('./petRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/pet', petRoutes);
+router.use('/post', postRoutes);
 
 module.exports = router;
