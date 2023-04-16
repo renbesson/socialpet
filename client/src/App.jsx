f3d8a6b7d9f9710e03517c36d2ba9509c3e7e663
Ren Besson <ren.besson@outlook.com>
2023-04-15 23:31:50 -0300

    HomePage added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/homepage" element={<HomePage />} />
               <Route path="/signin" element={<Signin />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/homepage" element={<HomePage />} />
