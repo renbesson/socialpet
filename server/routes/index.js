7ff5ff2b9ef929bd3cc55cf19a62260fdcdb6e63
Ren Besson <ren.besson@outlook.com>
2023-04-20 16:48:05 -0300

    Testing pages routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const pagesRoutes = require("./pages");
 
 router.use("/api", apiRoutes);
+router.use("/", pagesRoutes);
 
 router.use((req, res) => res.send("Wrong Route!"));
 
