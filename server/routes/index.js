e8ffb1d747f86ea2d6d2762def6945cc6fb7a925
Ren Besson <ren.besson@outlook.com>
2023-04-20 17:16:41 -0300

    Removed pages routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const pagesRoutes = require("./pages");
 
 router.use("/api", apiRoutes);
-router.use("/", pagesRoutes);
 
 router.use((req, res) => res.send("Wrong Route!"));
 
